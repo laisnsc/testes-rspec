11c72f0734528c9c30493186ef5b64a485647ee6
Laís Nascimento <lais.sdsn@gmail.com>
2019-03-19 11:09:06 -0300

    edit page modified

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' #dasdboard
-    resources :admins, only: [:index, :edit] #admin
+    resources :admins, only: [:index, :edit, :update] #admin
   end
   
   devise_for :admins
