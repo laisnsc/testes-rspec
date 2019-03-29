156118d9f733b46b117df9fe704dac36b70e443b
Laís Nascimento <lais.sdsn@gmail.com>
2019-03-29 09:26:12 -0300

    changed validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,12 +1,12 @@
 class Admin < ApplicationRecord
-  #EMAIL_REGEX = "^[a-z]$"
+  EMAIL_REGEX = /\A[a-zA-Z0-9]+@id.uff.br\z/
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :password, presence: true, length: { minimum: 8 }
-  #validates :email, presence: true, format: {with: /\A[a-zA-Z0-9]+\z/}
+  #validates :password, presence: true, length: { minimum: 8 }
+  validates :email, presence: true, format: {with: EMAIL_REGEX}
   #Kaminari
   paginates_per 5
 end
